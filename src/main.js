79160a24d939c5ab075bfb61e250756dbe97a5d7
Jason Lutterloh <jason.lutterloh@gmail.com>
2020-07-27 16:37:28 -0500

    initial pwa changes

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ const app = new App({
 	target: document.body
 });
 
+if ('serviceWorker' in navigator) {
+	// Use the window load event to keep the page load performant
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js');
+	});
+}
+
 export default app;
