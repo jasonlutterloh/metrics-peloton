3a25803476d979676e910e40b3de59989474f36a
Jason Lutterloh <jason.lutterloh@gmail.com>
2020-08-01 13:17:28 -0500

    Option Icon

--- src/options/OptionsButton.svelte
+++ src/options/OptionsButton.svelte
@@ -10,12 +10,33 @@
 </script>
 
 <div>
-    <button type="button" on:click={showModal}>Options</button>
+    <button type="button" on:click={showModal}><span class="sr-only">Options</span></button>
 </div>
 <style>
     div{
         position: absolute;
-        right: 20px;
+        right: 15px;
         top: 10px;
+    }
+    button{
+        background-image: url(/images/settings-icon.svg);
+        background-repeat: no-repeat;
+        background-size: 24px;
+        height: 30px;
+        width: 30px;
+        background-position: center center;
+        border: 0;
+        background-color: transparent;
+        transition: .5s all;
+        border-radius: 100%;
+        cursor: pointer;
+    }
+    button:hover,
+    button:focus{
+        transform: rotate(90deg);
+        background-color: #cccccc;
+    }
+    button:active{
+        background-color: #bbbbbb;
     }
 </style>
