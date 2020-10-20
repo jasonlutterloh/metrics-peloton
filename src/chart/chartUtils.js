9ead7f804b6e1b7d06bef5814d4cc606777d5b1f
Trevor Mack <drummer8001@gmail.com>
2020-10-20 14:47:52 -0400

    when getting color with a data list size > colors/pallete length currently always results in first color due to Math.floor(9/10)

--- src/chart/chartUtils.js
+++ src/chart/chartUtils.js
@@ -2,7 +2,7 @@ export const getColor = (lengthOfArray, index) => {
 	// Vaguely the colors of the rainbow. Idea is to gradually move up to red for the hardest rides.
 	const colorPalette = ["#e60000", "#ff4d88", "#e65c00", "#ffcc00", "#00cc00","#00cccc", "#008ae6", "#6666ff", "#7300e6"].reverse();
 	// Most people only do a few different distances, don't want it to always be the first three colors of the rainbow. This allows spreading through evenly depending on how many different lengths of ride the user has done.
-	let skip = Math.floor(colorPalette.length/lengthOfArray);  
+	let skip = Math.max(1, Math.floor(colorPalette.length/lengthOfArray));  
 	let arrayValue = index*skip;
 	let color = colorPalette[arrayValue];
 
