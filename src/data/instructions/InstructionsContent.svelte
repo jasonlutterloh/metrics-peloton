d93e682bd59ce79c88bcf782c4222b81412f07df
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2020-11-18 15:02:11 -0600

    Including missed files but still needs cleanup

---
+++ src/data/instructions/InstructionsContent.svelte
@@ -0,0 +1,27 @@
+<h1>Uploading Your Peloton Data</h1>
+<ol>
+    <li>
+        <a target="_blank" href="https://members.onepeloton.com/profile/workouts">Click here to view the Workouts section of your Peloton profile</a>
+        <p>(Open this link with the browser, not the mobile app)</p>
+    </li>
+    <li>Click the "Download Workouts" button. This will save a CSV file to your device.
+    </li>
+    <li>In the form, click "Choose File", select the file you just downloaded, and then click "Update"!</li>
+</ol>
+
+<style>
+    h1{
+        font-weight: 200;
+    }
+    ol{
+        padding-left: 20px;
+    }
+    li{
+        padding-bottom: 10px;
+    }
+    p {
+        display: inline-block;
+        font-style: italic;
+        margin: 0;
+    }
+</style>
