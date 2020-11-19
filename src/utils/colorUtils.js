bd85c6764726c1b22e8b39a551234abf07d7ff59
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2020-11-19 12:45:43 -0600

    Changing color step to rainbow from blue

--- src/utils/colorUtils.js
+++ src/utils/colorUtils.js
@@ -1,18 +1,29 @@
 export const GREY = "#bbbbbb";
-
-export const colors = {
-  // maximumbluegreen: "#3fc1c0ff",
-  maximumbluegreen2: "#20bac5ff",
-  // pacificblue: "#00b2caff",
-  pacificblue2: "#04a6c2ff",
-  bluegreen: "#0899baff",
-  cgblue: "#0f80aaff",
-  sapphireblue: "#16679aff",
-  lapislazuli: "#1a5b92ff",
-  usafablue: "#1c558eff",
-  yaleblue: "#1d4e89ff",
-  prussianblue: "#012A4A",
+export const colors ={
+  purpleplum: "#A44DD0",
+  darkslateblue: "#54478cff",
+  sapphireblue: "#2c699aff",
+  bluemunsell: "#048ba8ff",
+  keppel: "#0db39eff",
+  darkpastelgreen: "#3DBC45",
+  princetonorange: "#E87C24",
+  burntsienna: "#ee6c4dff",
+  radicalred: "#FF1654",
+  vividburgundy: "#9e0031ff",
 };
+// export const colors = {
+//   // maximumbluegreen: "#3fc1c0ff",
+//   maximumbluegreen2: "#20bac5ff",
+//   // pacificblue: "#00b2caff",
+//   pacificblue2: "#04a6c2ff",
+//   bluegreen: "#0899baff",
+//   cgblue: "#0f80aaff",
+//   sapphireblue: "#16679aff",
+//   lapislazuli: "#1a5b92ff",
+//   usafablue: "#1c558eff",
+//   yaleblue: "#1d4e89ff",
+//   prussianblue: "#012A4A",
+// };
 export const getDefaultColorArray = () => {
   const keys = Object.keys(colors);
   const array = [];
