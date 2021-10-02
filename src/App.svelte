784dc009a455b7ea1f32190dff0ba65d3a77b91d
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-10-02 11:40:27 -0500

    eslint fix

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,15 @@
 	import Sidebar from "./options/Sidebar.svelte";
 	import MainContent from "./components/MainContent.svelte";
 	import {onMount} from "svelte";
-	import { firebaseConfig } from "./firebase.config";
-	import { initializeApp } from "firebase/app";
-	import { getAnalytics } from "firebase/analytics";
+	import {firebaseConfig} from "./firebase.config";
+	import {initializeApp} from "firebase/app";
+	import {getAnalytics} from "firebase/analytics";
 	
 	onMount(async () => {
-		if (firebaseConfig?.projectId){
-			const app = initializeApp(firebaseConfig);
-			const firebaseAnalytics = getAnalytics(app);
-		}
+	  if (firebaseConfig?.projectId) {
+	    const app = initializeApp(firebaseConfig);
+	    getAnalytics(app);
+	  }
 	});
 	let isSidebarOpen = false;
 </script>
