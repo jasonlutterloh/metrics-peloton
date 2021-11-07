b48bd268cbab85e9af88f5437afd717b85b67e85
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2021-11-07 09:09:13 -0600

    Remove firebase config code

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,6 @@
 	import Footer from "./components/Footer.svelte";
 	import Sidebar from "./options/Sidebar.svelte";
 	import MainContent from "./components/MainContent.svelte";
-	import {onMount} from "svelte";
-	import {firebaseConfig} from "./firebase.config";
-	import {initializeApp} from "firebase/app";
-	import {getAnalytics} from "firebase/analytics";
-	
-	onMount(async () => {
-	  if (firebaseConfig?.projectId) {
-	    const app = initializeApp(firebaseConfig);
-	    getAnalytics(app);
-	  }
-	});
 	let isSidebarOpen = false;
 </script>
 
