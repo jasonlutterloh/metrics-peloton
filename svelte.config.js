5c123045c6d66d72fb3c4be75da9df6cdb260026
jasonlutterloh <22846656+jasonlutterloh@users.noreply.github.com>
2023-08-28 20:13:28 -0500

    Change build folder

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
+      pages: "public",
+      assets: "public",
       fallback: "app.html",
       precompress: false,
       strict: true
